a031a7dd2f09fc89b145ae47cb28c32b5a586a43
ForJackWu <437106491@qq.com>
2025-09-23 10:01:11 +0800

    burger

--- helloWorld.py
+++ helloWorld.py
@@ -2,6 +2,7 @@ hungry = input("are you Hungry?")
 if hungry == "yes":
     print("eat samosa")
     print("eat pizza")
+    print("eat burger")
 else:
     print("watch TV show")
     print("do your home work")
